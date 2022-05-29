b3e34e41ff5479dc4dc2e98d1e2221d12b015e26
Resh@308 <reshsingh1995@gmail.com>
2022-05-30 03:36:47 +0530

    printing hello from bridgelabzs

---
+++ src/main/java/com/bridgelabz/CfpDay2Application.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfpDay2Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from bridgelabz");
+        SpringApplication.run(CfpDay2Application.class, args);
+    }
+
+}
